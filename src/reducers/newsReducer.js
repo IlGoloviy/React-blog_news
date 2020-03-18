d32796b5afc010df6fe5fe812296b26b35a7f989
Illia Holovii <goloviyillya@gmail.com>
2020-03-18 19:24:53 +0200

    Updates

---
+++ src/reducers/newsReducer.js
@@ -0,0 +1,37 @@
+export default function reducer(state = {
+    news: [],
+    fetching: false,
+    fetched: false,
+    error: null
+  }, action) {
+    
+    switch (action.type) {
+      case 'FETCH_NEWS': {
+        return {...state, fetching: true}
+      }
+      case 'FETCH_NEWS_REJECTED': {
+        return {
+          ...state, 
+          fetching: false, 
+          error: action.payload
+        }
+      }
+      case 'FETCH_NEWS_FULFILLED': {
+        return {
+          ...state, 
+          fetching: false, 
+          fetched: true, 
+          news: action.payload.articles
+        }
+      }
+      case 'ADD_NEWS': {
+        return {
+          ...state,
+          news: [action.payload, ...state.news]
+        }
+      }
+      
+    }
+  
+    return state;
+  }
